eff38217e7265a0edfd834ff6206ce0e96ea0330
willcl-ark <will@256k1.dev>
2025-03-10 12:25:51 +0000

    add git config to home manager

--- home/will/home.nix
+++ home/will/home.nix
@@ -74,10 +74,11 @@
 
     git = {
       enable = true;
+      delta.enable = true;
       userName = "will";
       userEmail = "will@256k1.dev";
       signing = {
-        key = null; # Let GPG decide which key to use
+        key = "0xCE6EC49945C17EA6";
         signByDefault = true;
       };
       extraConfig = {
@@ -88,10 +89,64 @@
         gpg.program = "${pkgs.gnupg}/bin/gpg2";
       };
       aliases = {
-        st = "status";
-        co = "checkout";
-        c = "commit";
+        # Common commands
+        a = "add .";
         b = "branch";
+        cp = "cherry-pick";
+        cpcont = "cherry-pick --continue";
+        d = "difftool";
+        ds = "diff --staged";
+        f = "fetch --all --prune";
+        lo = "log --oneline -n 20";
+        m = "mergetool";
+        po = "push origin";
+        pu = "push upstream";
+        pushf = "push --force-with-lease";
+        r = "rebase";
+        ra = "rebase --abort";
+        rcont = "rebase --continue";
+        rd = "range-diff";
+        rem = "remote";
+        rh = "reset --hard";
+        s = "status";
+
+        # File Management
+        co = "checkout";
+        cob = "checkout -b";
+        un = "reset HEAD";
+
+        # Commit Commands
+        amend = "commit --amend";
+        cm = "commit";
+        com = "commit -m";
+        fix = "commit --amend --no-edit";
+
+        # Custom scripts
+        show-pr =
+          "!f() { git log --merges --ancestry-path --oneline $1..HEAD | tail -n 1; }; f";
+        ack = "!f() { git rev-parse HEAD | tr -d '[:space:]' | xsel -b; }; f";
+        files = "!f() { git diff-tree --no-commit-id --name-only -r HEAD; }; f";
+        fixup =
+          "!git log -n 50 --pretty=format:'%h %s' --no-merges | fzf | cut -c -7 | xargs -o git commit --fixup";
+        last = "log -1 HEAD";
+        rb =
+          "!f() { default_branch=$(git symbolic-ref refs/remotes/upstream/HEAD | sed 's@^refs/remotes/upstream/@@'); git rebase -i $(git merge-base HEAD upstream/$default_branch); }; f";
+        rba =
+          "!f() { default_branch=$(git symbolic-ref refs/remotes/upstream/HEAD | sed 's@^refs/remotes/upstream/@@'); git rebase -i $(git merge-base HEAD upstream/$default_branch) --autosquash; }; f";
+        review =
+          "!f() { git -c sequence.editor='sed -i s/pick/edit/' rebase -i $(git merge-base master HEAD); }; f";
+        tags =
+          "!sh -c 'git for-each-ref --sort=-taggerdate --format=\"%(refname:lstrip=2)\" refs/tags | fzf | xargs git checkout'";
+      };
+      delta.options = {
+        decorations = {
+          commit-decoration-style = "bold yellow box ul";
+          commit-style = "raw";
+          file-decoration-style = "none";
+          file-style = "bold yellow ul";
+        };
+        features = "line-numbers decorations mellow-barbet";
+        whitespace-error-style = "22 reverse";
       };
     };
 
