3244f57d4c1b6efb2551611db705e35530867503
will <will@256k1.dev>
2025-06-23 20:36:57 +0100

    use overlay driver for container storage

--- modules/virtualization.nix
+++ modules/virtualization.nix
@@ -21,31 +21,35 @@ in {
     };
   };
 
-  config = {
-    virtualisation.libvirtd = mkIf cfg.enableKvm {
-      enable = true;
-      qemu = {
-        package = pkgs.qemu_kvm;
-        runAsRoot = true;
-        ovmf.enable = true;
+  config = mkMerge [
+    (mkIf cfg.enablePodman {
+      virtualisation.podman = {
+        enable = true;
+        dockerCompat = true;
+        dockerSocket.enable = true;
+        defaultNetwork.settings.dns_enabled = true;
       };
-    };
+      virtualisation.containers = {
+        enable = true;
+        storage.settings.storage = {
+          driver = "overlay";
+          graphroot = "/var/lib/containers/storage";
+          runroot = "/run/containers/storage";
+        };
+      };
+    })
 
-    virtualisation.podman = mkIf cfg.enablePodman {
-      enable = true;
-      dockerCompat = true;
-      dockerSocket.enable = true;
-      defaultNetwork.settings.dns_enabled = true;
-    };
-
-    # Add users to necessary groups
-    users.groups = mkMerge [
-      (mkIf cfg.enableKvm {libvirtd = {};})
-    ];
-
-    # Add necessary packages
-    environment.systemPackages = mkMerge [
-      (mkIf cfg.enableKvm (with pkgs; [virt-manager spice-gtk]))
-    ];
-  };
+    (mkIf cfg.enableKvm {
+      virtualisation.libvirtd = {
+        enable = true;
+        qemu = {
+          package = pkgs.qemu_kvm;
+          runAsRoot = true;
+          ovmf.enable = true;
+        };
+      };
+      users.groups.libvirtd = {};
+      environment.systemPackages = with pkgs; [ virt-manager spice-gtk ];
+    })
+  ];
 }
