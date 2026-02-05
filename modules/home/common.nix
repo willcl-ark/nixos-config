265a10e166ac49a0ba73d9b8ae26b6c4b6f13f08
will <will@256k1.dev>
2026-02-05 10:00:36 +0000

    sort packages

--- modules/home/common.nix
+++ modules/home/common.nix
@@ -7,62 +7,60 @@
 
       home.enableNixpkgsReleaseCheck = false;
 
-      home.packages =
-        lib.optionals pkgs.stdenv.hostPlatform.isLinux [ pkgs.elfutils ]
-        ++ [
-          pkgs.actionlint
-          pkgs.asciinema
-          pkgs.bash
-          pkgs.clang_19
-          pkgs.claude-code
-          pkgs.cmake
-          pkgs.codex
-          pkgs.curl
-          pkgs.delta
-          pkgs.difftastic
-          pkgs.fastfetch
-          pkgs.fd
-          pkgs.findutils
-          pkgs.fzf
-          pkgs.gh-dash
-          pkgs.git
-          pkgs.gitlint
-          pkgs.gnugrep
-          pkgs.gnumake
-          pkgs.gnused
-          pkgs.gnutar
-          pkgs.go
-          pkgs.htop
-          pkgs.jq
-          pkgs.lazygit
-          pkgs.lua
-          pkgs.lua54Packages.luacheck
-          pkgs.lynx
-          pkgs.markdownlint-cli
-          pkgs.mold
-          pkgs.ninja
-          pkgs.notmuch-addrlookup
-          pkgs.opencode
-          pkgs.podman
-          pkgs.python312
-          pkgs.qemu
-          pkgs.ripgrep
-          pkgs.ripmime
-          pkgs.rsync
-          pkgs.ruff
-          pkgs.rustup
-          pkgs.shellcheck
-          pkgs.sqlite
-          pkgs.tmux
-          pkgs.tokei
-          pkgs.unzip
-          pkgs.urlscan
-          pkgs.uv
-          pkgs.wget
-          pkgs.yamllint
-          pkgs.yt-dlp
-          pkgs.zig
-        ];
+      home.packages = lib.optionals pkgs.stdenv.hostPlatform.isLinux [ pkgs.elfutils ] ++ [
+        pkgs.actionlint
+        pkgs.asciinema
+        pkgs.bash
+        pkgs.clang_19
+        pkgs.claude-code
+        pkgs.cmake
+        pkgs.codex
+        pkgs.curl
+        pkgs.delta
+        pkgs.difftastic
+        pkgs.fastfetch
+        pkgs.fd
+        pkgs.findutils
+        pkgs.fzf
+        pkgs.gh-dash
+        pkgs.git
+        pkgs.gitlint
+        pkgs.gnugrep
+        pkgs.gnumake
+        pkgs.gnused
+        pkgs.gnutar
+        pkgs.go
+        pkgs.htop
+        pkgs.jq
+        pkgs.lazygit
+        pkgs.lua
+        pkgs.lua54Packages.luacheck
+        pkgs.lynx
+        pkgs.markdownlint-cli
+        pkgs.mold
+        pkgs.ninja
+        pkgs.notmuch-addrlookup
+        pkgs.opencode
+        pkgs.podman
+        pkgs.python312
+        pkgs.qemu
+        pkgs.ripgrep
+        pkgs.ripmime
+        pkgs.rsync
+        pkgs.ruff
+        pkgs.rustup
+        pkgs.shellcheck
+        pkgs.sqlite
+        pkgs.tmux
+        pkgs.tokei
+        pkgs.unzip
+        pkgs.urlscan
+        pkgs.uv
+        pkgs.wget
+        pkgs.yamllint
+        pkgs.yt-dlp
+        pkgs.zig
+      ];
 
       programs.ripgrep = {
         enable = true;
