2b033c4b55cb15c5897116d4b1f68b594b5a19c4
will <will@256k1.dev>
2026-03-07 00:04:23 +0000

    REVERTME: fixup broken guix build

--- modules/services/guix.nix
+++ modules/services/guix.nix
@@ -3,6 +3,16 @@
   flake.modules.nixos.desktop-services =
     { pkgs, ... }:
     {
+      nixpkgs.overlays = [
+        (_: prev: {
+          guile-zlib = prev.guile-zlib.overrideAttrs {
+            doCheck = false;
+            postConfigure = ''
+              sed -i "s|/nix/store/[a-z0-9]*-zlib-[^\"]*-static/lib/libz|${prev.zlib.out}/lib/libz|" zlib/config.scm
+            '';
+          };
+        })
+      ];
       environment.systemPackages = [ pkgs.guix ];
       services.guix.enable = true;
     };
